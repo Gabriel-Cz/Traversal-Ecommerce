3d33c3a0e5d4ff62ae193e974dd3aba386d6f43b
Gabriel Cazares <gabrielczhz@gmail.com>
2021-05-04 18:04:24 -0500

    testing axios different config

--- store/actions/productsActions.js
+++ store/actions/productsActions.js
@@ -1,40 +1,15 @@
 import axios from 'axios'
 import * as types from '../types'
 
-
-/*
 const reqConfig = {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit,
+    mode: 'cors', 
+    cache: 'no-cache', 
+    credentials: 'same-origin', 
     headers: {
       'Content-Type': 'application/json',
       // 'Content-Type': 'application/x-www-form-urlencoded',
     }
 }
-
-let fetchPostJSON = async (url) => {
-    try {
-        // Default options are marked with *
-        const response = await fetch(url, {
-          method: 'GET', // *GET, POST, PUT, DELETE, etc.
-          mode: 'cors', // no-cors, *cors, same-origin
-          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: 'same-origin', // include, *same-origin, omit
-          headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          redirect: 'follow', // manual, *follow, error
-          referrerPolicy: 'no-referrer', // no-referrer, *client
-          body: JSON.stringify(data || {}), // body data type must match "Content-Type" header
-        });
-        return await response.json(); // parses JSON response into native JavaScript objects
-      } catch (err) {
-        throw new Error(err.message);
-      }
-}*/
 
 export const getProducts = () => async (dispatch) => {
     let response = await axios.get('http://localhost:3000/api/products', reqConfig)
@@ -57,14 +32,9 @@ export const getProduct = (id) => async (dispatch) => {
 }
 
 export const filterProducts = (type, filter) => async (dispatch) => {
-    /* let response = await axios.get('http://localhost:3000/api/products', reqConfig)
+    let response = await axios.get('http://localhost:3000/api/products', reqConfig)
                   .then(res => res.data)
-                  .catch(e => console.log(e)); */
-    let response = [
-        {
-            title: "asd"
-        }
-    ]
+                  .catch(e => console.log(e));
     dispatch({
         type: types.FILTER_PRODUCTS,
         payload: response.filter(product => product[type] === filter),
