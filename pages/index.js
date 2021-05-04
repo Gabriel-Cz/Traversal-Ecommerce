5999e3555541da85ceb834a92be164fde437a1de
Gabriel Cazares <gabrielczhz@gmail.com>
2021-05-04 00:21:51 -0500

    correcting more imported files

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,25 @@ import React from 'react'
 import FirstSection from '../components/HomePageComponents/FirstSection'
 import TrendingSection from '../components/HomePageComponents/TrendingSection'
 import GoldAndSilverSection from '../components/HomePageComponents/GoldAndSilverSection'
-import ShipmentsInformation from '../components/ShipmentsInformation'
-import CustomersReviewsSection from '../components/CustomersReviewsSection'
+import CustomersReviewsSection from '../components/HomePageComponents/CustomersReviewsSection'
+import ShipmentsInformation from '../components/HomePageComponents/ShipmentsInformation'
 import { wrapper } from '../store' 
 import { filterProducts } from '../store/actions/productsActions'
-import { useSelector } from 'react-redux'
-import styles from '../styles/Home.module.scss'
 
 export const getServerSideProps = wrapper.getServerSideProps(async ({store}) => {
   await store.dispatch(filterProducts('state', 'trending'))
+  return {
+    props: {
+      trendingProducts: store.getState().productsReducer.server.filteredProducts
+    }
+  }
 })
 
-export function Home() {
-  const {filteredProducts} = useSelector(state => state.productsReducer.server)
+export function Home({trendingProducts}) {
   return (
     <>
       <FirstSection></FirstSection>
-      <TrendingSection trendingProducts={filteredProducts}></TrendingSection>
+      <TrendingSection trendingProducts={trendingProducts}></TrendingSection>
       <GoldAndSilverSection></GoldAndSilverSection>
       <CustomersReviewsSection></CustomersReviewsSection>
       <ShipmentsInformation></ShipmentsInformation>
