df4872d14518e9a1baaa1ecdf3820b44c2a7d524
Gabriel Cazares <gabrielczhz@gmail.com>
2021-05-04 19:07:43 -0500

    changes in config file async rewrites function for api

--- next.config.js
+++ next.config.js
@@ -14,4 +14,12 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*',
+      },
+    ]
+  }
 }
